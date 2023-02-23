92be57aae4f556ebd6f2a54f0f8358a6dff59f70
germankern <germankern890@gmail.com>
2023-02-23 17:43:34 -0300

    iniciando app e instalando reactDom y ReactRouterDom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logoNuevo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Citas Rápidas en Santiago de Chile y Viña del Mar
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://faceitcitasrapidas.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Face It 
         </a>
       </header>
     </div>

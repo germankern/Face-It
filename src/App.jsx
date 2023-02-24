e21272f3bce653097b45d806cdd705c2feb5fa2b
germankern <germankern890@gmail.com>
2023-02-24 18:27:09 -0300

    add component ButtonLinksContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import ButtonLinksContainer from './components/ButtonLinksContainer';
 import DescriptionSection from './components/DescriptionSection';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <p>Aqui va el Navbar</p>
       <DescriptionSection></DescriptionSection>
+      <ButtonLinksContainer></ButtonLinksContainer>
     </div>
   );
 }

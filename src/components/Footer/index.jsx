3f3ce61a978fe2b8c24b593edb31299b293cee41
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-03-01 12:23:36 -0300

    Recompuse el footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Contact from "../Contact";
+import Networks from "../Networks";
+import Copyright from "../Copyright";
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <>
+      <div className={styles.informationContainer}>
+        <Contact />
+        <Networks />
+      </div>
+      <Copyright />
+    </>
+  );
+}
+
+export default Footer;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar";
-import Contact from "../Contact";
-import Networks from "../Networks";
-import Copyright from "../Copyright";
 import styles from "./styles.module.css";
+import Footer from "../Footer";
 
 function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Navbar />
       {children}
-      <div className="information-container">
-        <Contact />
-        <Networks />
-      </div>
-      <Copyright />
+      <Footer />
     </div>
   );
 }

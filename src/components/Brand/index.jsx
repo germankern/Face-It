d7b78051fecd5882eab19523073f8b883c431fb5
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-03-02 00:42:12 -0500

    Navbar Complete Part-I

--- src/components/Brand/index.jsx
+++ src/components/Brand/index.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import IconLogo from "../../assets/images/IconLogo.svg";
 import Logo from "../../assets/images/Logo.svg";
+import styles from "./styles.module.css";
 
-const Brand = (props) => {
+const Brand = () => {
   return (
     <div
       style={{ display: "flex", alignItems: "center" }}
-      className={props.name}
+      className={styles.containerBrand}
     >
       <Image style={{ marginRight: "6px" }} src={IconLogo} alt="IconLogo" />
       <Image src={Logo} alt="Logo" />
---
+++ src/pages/survey/index.jsx
@@ -0,0 +1,8 @@
+const Survey=()=>{
+    return(
+        <div>
+            Hola Soy Survey
+        </div>
+    )
+}
+export default Survey

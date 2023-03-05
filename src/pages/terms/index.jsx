4a3adb0d1e260d51ded9baf58905a60a700d387d
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-03-05 18:24:05 -0500

    Completed Terms

--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -1,3 +1,4 @@
+import BlockStyle from "@/components/BlockStyle/BlockStyle";
 import TextSection from "@/components/TextSection/TextSection";
 import styles from "./style.module.css";
 
@@ -113,38 +114,44 @@ const alltext = [
 
 const Survey = () => {
   return (
-    <div className={styles.TermsContainer}>
-      <div className={styles.IntroductionContainer}>
-        <h1>Terminos y Condiciones</h1>
-        <p>
-          Estos términos y condiciones describen las reglas y regulaciones para
-          el uso del sitio web de Face It! Citas Rápidas, ubicado en
-          https://faceitcitasrapidas.com/.
-        </p>
-        <p>
-          Al acceder a este sitio web, asumimos que aceptas estos términos y
-          condiciones. No continúes usando Face It! Citas Rápidas si no estás de
-          acuerdo con todos los términos y condiciones establecidos en esta
-          página.
-        </p>
+    <>
+      <div className={styles.TermsContainer}>
+        <div className={styles.IntroductionContainer}>
+          <h1>Terminos y Condiciones</h1>
+          <p>
+            Estos términos y condiciones describen las reglas y regulaciones
+            para el uso del sitio web de Face It! Citas Rápidas, ubicado en
+            https://faceitcitasrapidas.com/.
+          </p>
+          <p>
+            Al acceder a este sitio web, asumimos que aceptas estos términos y
+            condiciones. No continúes usando Face It! Citas Rápidas si no estás
+            de acuerdo con todos los términos y condiciones establecidos en esta
+            página.
+          </p>
+        </div>
+        <section className={styles.AllTextSection}>
+          {alltext.map((section, key) => (
+            <TextSection
+              key={key}
+              id={section.id}
+              title={section.title}
+              allParagraph={section.allParagraph}
+              allParagraph2={section.allParagraph2}
+              allParagraph3={section.allParagraph3}
+              allParagraph4={section.allParagraph4}
+              allList={section.allList}
+              allList2={section.allList2}
+              allList3={section.allList3}
+            />
+          ))}
+        </section>
       </div>
-      <section className={styles.AllTextSection}>
-        {alltext.map((section, key) => (
-          <TextSection
-            key={key}
-            id={section.id}
-            title={section.title}
-            allParagraph={section.allParagraph}
-            allParagraph2={section.allParagraph2}
-            allParagraph3={section.allParagraph3}
-            allParagraph4={section.allParagraph4}
-            allList={section.allList}
-            allList2={section.allList2}
-            allList3={section.allList3}
-          />
-        ))}
-      </section>
-    </div>
+      <BlockStyle
+        parrafo="Si quieres saber más, contactanos: "
+        email="info@faceitcitasrápidas.com"
+      />
+    </>
   );
 };
 

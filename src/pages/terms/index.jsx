8e15f6c926120464e71984d3810294fa3eab6075
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-03-05 10:30:29 -0500

    advance2

--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -24,12 +24,49 @@ const alltext = [
     },
     allParagraph2: {
       0: "Este acuerdo comenzará el fecha presente.",
-      1: "",
-      2: "",
+      1: "Partes de este sitio web ofrecen a los usuarios la oportunidad de publicar e intercambiar opiniones e información en determinadas áreas. Face It! Citas Rápidas no filtra, edita, publica ni revisa los comentarios antes de su presencia en el sitio web. Los comentarios no reflejan los puntos de vista ni las opiniones de Face It! Citas Rápidas, sus agentes y/o afiliados. Los comentarios reflejan los puntos de vista y opiniones de la persona que publica. En la medida en que lo permitan las leyes aplicables, Face It! Citas Rápidas no será responsable de los comentarios ni de ninguna responsabilidad, daños o gastos causados ​​o sufridos como resultado de cualquier uso o publicación o apariencia de comentarios en este sitio web.",
+      2: "Face It! Citas Rápidas se reserva el derecho de monitorear todos los comentarios y eliminar los que puedan considerarse inapropiados, ofensivos o que incumplan estos Términos y Condiciones.",
+      3: "Garantizas y declaras que:",
+    },
+    allList2: {
+      0: "Tienes derecho a publicar comentarios en nuestro sitio web y tienes todas las licencias y consentimientos necesarios para hacerlo;",
+      1: "Los comentarios no invaden ningún derecho de propiedad intelectual, incluidos, entre otros, los derechos de autor, patentes o marcas comerciales de terceros;",
+      2: "Los comentarios no contienen ningún material difamatorio, calumnioso, ofensivo, indecente o ilegal de otro modo, que sea una invasión de la privacidad.",
+      3: "Los comentarios no se utilizarán para solicitar o promover negocios o actividades comerciales personalizadas o presentes o actividades ilegales.",
+      4: "Por la presente, otorgas a Face It! Citas Rápidas una licencia no exclusiva para usar, reproducir, editar y autorizar a otros a usar, reproducir y editar cualquiera de tus comentarios en todas y cada una de las formas, formatos, o medios.",
     },
   },
   {
     title: "Hipervínculos a nuestro contenido:",
+    allParagraph: {
+      0: "Las siguientes organizaciones pueden vincularse a nuestro sitio web sin aprobación previa por escrito:",
+    },
+    allList: {
+      0: "Agencias gubernamentales;",
+      1: "Motores de búsqueda;",
+      2: "Organizaciones de noticias;",
+      3: "Los distribuidores de directorios en línea pueden vincularse a nuestro sitio web de la misma manera que hacen hipervínculos a los sitios web de otras empresas que figuran en la lista;",
+      4: "Empresas acreditadas en todo el sistema, excepto que soliciten organizaciones sin fines de lucro, centros comerciales de caridad y grupos de recaudación de fondos de caridad que no pueden hacer hipervínculos a nuestro sitio web.",
+    },
+    allParagraph2: {
+      0: "Estas organizaciones pueden enlazar a nuestra página de inicio, a publicaciones o a otra información del sitio siempre que el enlace: (a) no sea engañoso de ninguna manera; (b) no implique falsamente patrocinio, respaldo o aprobación de la parte vinculante y sus productos y/o servicios; y (c) encaja en el contexto del sitio de la parte vinculante.",
+      1: "Podemos considerar y aprobar otras solicitudes de enlaces de los siguientes tipos de organizaciones:",
+    },
+    allList2: {
+      0: "fuentes de información de consumidores y/o empresas comúnmente conocidas;",
+      1: "sitios de la comunidad .com;",
+      2: "asociaciones u otros grupos que representan organizaciones benéficas;",
+      3: "distribuidores de directorios en línea;",
+      4: "portales de Internet;",
+      5: "firmas de contabilidad, derecho y consultoría; y",
+      6: "instituciones educativas y asociaciones comerciales.",
+    },
+    allParagraph3: {
+      0: "Aprobaremos las solicitudes de enlace de estas organizaciones si: (a) el enlace no nos haría vernos desfavorablemente a nosotros mismos ni a nuestras empresas acreditadas; (b) la organización no tiene registros negativos con nosotros; (c) el beneficio para nosotros de la visibilidad del hipervínculo compensa la ausencia de Face It! Citas Rápidas; y (d) el enlace está en el contexto de información general de recursos.",
+      1: "Estas organizaciones pueden enlazar a nuestra página de inicio siempre que el enlace: (a) no sea engañoso de ninguna manera; (b) no implique falsamente patrocinio, respaldo o aprobación de la parte vinculante y sus productos o servicios; y (c) encaja en el contexto del sitio de la parte vinculante",
+      2: "Si eres una de las organizaciones enumeradas en el párrafo 2 y estás interesada en vincularte a nuestro sitio web, debes informarnos enviando un correo electrónico a Face It! Citas Rápidas. Incluye tu nombre, el nombre de tu organización, la información de contacto, así como la URL de tu sitio, una lista de las URL desde las que tienes la intención de vincular a nuestro sitio web y una lista de las URL de nuestro sitio a las que te gustaría acceder. Espera 2-3 semanas para recibir una respuesta.",
+      3: "Las organizaciones aprobadas pueden hacer hipervínculos a nuestro sitio web de la siguiente manera:",
+    },
   },
   {
     title: "Responsabilidad del contenido:",

f0fc25e487768ae51757786bbf98682b21470fa1
germankern <germankern890@gmail.com>
2023-03-13 16:39:01 -0300

    create rutes in pages/faq

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import FAQmobile from "@/components/FAQmobile";
+import FAQdesktop from "@/components/FAQdesktop";
+import styles from "./style.module.css";
 
 export default function FaqPage() {
   return (
     <>
-      <FAQmobile />
+      <div className={styles.mobileFAQ}>
+        <FAQmobile />
+      </div>
+      <div className={styles.desktopFAQ}>
+        <FAQdesktop />
+      </div>
     </>
   );
 }

95761acfc4db3edede8520d4768a0f6898563ae5
germankern <germankern890@gmail.com>
2023-03-13 15:07:34 -0300

    rename fn faqPage to FaqPage in pages/faq

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FAQmobile from "@/components/FAQmobile";
 
-export default function faqPage() {
+export default function FaqPage() {
   return (
     <>
       <FAQmobile />

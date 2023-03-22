1ab00f3af19901c4a22b57a20103798a6c9eec0d
germankern <germankern890@gmail.com>
2023-03-22 12:20:09 -0300

    EventosDesktop and EventosMobile done 100% funtional

--- src/pages/eventos/index.jsx
+++ src/pages/eventos/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-// import EventosMobile from "@/components/EventosMobile";
+import EventosMobile from "@/components/EventosMobile";
 import EventosDesktop from "@/components/EventosDesktop";
+import style from "./style.module.css";
 
 export default function EventosPage() {
   return (
     <>
-      {/* <EventosMobile /> */}
-      <EventosDesktop />
+      <div className={style.mobile}>
+        <EventosMobile />
+      </div>
+      <div className={style.desktop}>
+        <EventosDesktop />
+      </div>
     </>
   );
 }

bcfb91bcfc3688ff93dfd546941a5bdc2b84a14e
germankern <germankern890@gmail.com>
2023-03-23 16:03:03 -0300

    fix Networks and add links

--- src/components/Networks/index.jsx
+++ src/components/Networks/index.jsx
@@ -7,18 +7,42 @@ import Facebook2 from "../../assets/images/facebookWhite.svg";
 import Tiktok2 from "../../assets/images/titokWhite.svg";
 import Instagram2 from "../../assets/images/instagramWhite.svg";
 import { useRouter } from "next/router";
+
 const Networks = () => {
   const router = useRouter();
   const eventos = router.pathname === "/eventos";
   const FacebookImageSrc = eventos ? Facebook2 : Facebook;
   const InstagramImageSrc = eventos ? Instagram2 : Instagram;
   const TiktokImageSrc = eventos ? Tiktok2 : Tiktok;
+
+  const handleFacebookClick = () => {
+    window.open("https://www.facebook.com/", "_blank");
+  };
+  const handleInstagramClick = () => {
+    window.open("https://www.instagram.com/faceitcitasrapidas/", "_blank");
+  };
+  const handleTiktokClick = () => {
+    window.open("https://www.tiktok.com/", "_blank");
+  };
+
   return (
     <div className={styles.containerNetworks}>
       <h4>Redes</h4>
-      <Image src={FacebookImageSrc} alt="Facebook" />
-      <Image src={InstagramImageSrc} alt="Instagram" />
-      <Image src={TiktokImageSrc} alt="Tiktok" />
+      <Image
+        src={FacebookImageSrc}
+        alt="Facebook logo"
+        onClick={handleFacebookClick}
+      />
+      <Image
+        src={InstagramImageSrc}
+        alt="Instagram logo"
+        onClick={handleInstagramClick}
+      />
+      <Image
+        src={TiktokImageSrc}
+        alt="Tiktok logo"
+        onClick={handleTiktokClick}
+      />
     </div>
   );
 };

15a459d3b17454beedc34ed425a20c0b4f71b708
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-03-27 19:32:26 -0300

    Add export script

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: process.env.NODE_ENV === "production" ? "export" : "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;

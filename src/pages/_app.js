ff442bc245ffbca3452daa8fd6b730a0d115d512
germankern <germankern890@gmail.com>
2023-04-15 15:46:42 -0300

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,24 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
+// import Script from "next/script";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      {/* <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LXQ92YMNJT"
+      />
+      <script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LXQ92YMNJT');
+        `}
+      </script> */}
+      <GoogleAnalytics measurementId="G-LXQ92YMNJT" />
       <Component {...pageProps} />
     </Layout>
   );
